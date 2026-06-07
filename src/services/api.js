d42e2b0ec792564a946588d25e5e5ee39faca6ac
theAmrit07 <125031015+theAmrit07@users.noreply.github.com>
2026-06-07 22:01:05 +0545

    Connect frontend to production backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://127.0.0.1:8000/api'
+const API_URL = 'https://smart-hire-production-283f.up.railway.app/api'
 
 const api = axios.create({
   baseURL: API_URL,
